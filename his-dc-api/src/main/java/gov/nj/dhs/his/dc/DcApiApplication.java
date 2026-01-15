eced9032619168d4a0a42a1337d483837365c6b8
Vamsikrishna Chinthakayala <vamsikrishnach2000@gmail.com>
2026-01-15 17:39:54 +0530

    redis cache added

--- his-dc-api/src/main/java/gov/nj/dhs/his/dc/DcApiApplication.java
+++ his-dc-api/src/main/java/gov/nj/dhs/his/dc/DcApiApplication.java
@@ -2,11 +2,13 @@ package gov.nj.dhs.his.dc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCaching
 @ComponentScan(basePackages = "gov.nj.dhs.his")
 public class DcApiApplication {
 
